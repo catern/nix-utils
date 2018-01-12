0e1889a82d3b74202de8cc2547816e60a4ced57b
Spencer Baugh <sbaugh@catern.com>
2018-01-12 17:34:18 +0000

    dpkg: add symlinks to the package, don't create them in postinst

--- dpkg/default.nix
+++ dpkg/default.nix
@@ -1,4 +1,4 @@
-{ binutils, runCommand, fpm }:
+{ runCommand, nix, perl, pathsFromGraph, shellcheck, fpm, binutils }:
 let
   sharball = import ../tarball/doit.nix;
 in
@@ -6,8 +6,34 @@ runCommand "nix.deb" {
   buildInputs = [ fpm binutils ];
 }
   ''
-    mkdir -p $TMPDIR/usr/local/lib/nix/
-    cp ${sharball} $TMPDIR/usr/local/lib/nix/bootstrap.tar.bz.sh
-    fpm -s dir -t deb -n nix --post-install ${./install.sh} -p $out -C $TMPDIR usr/
+    nix=${nix}
+    cd $TMPDIR
+
+    # copy in the Nix bootstrap sharball
+    mkdir -p usr/lib/nix/
+    cp ${sharball} usr/lib/nix/bootstrap.tar.bz.sh
+
+    # create links to systemd unit files
+    mkdir -p usr/lib/systemd/system/
+    ln -sf $nix/lib/systemd/system/nix-daemon.socket usr/lib/systemd/system/nix-daemon.socket
+    ln -sf $nix/lib/systemd/system/nix-daemon.service usr/lib/systemd/system/nix-daemon.service
+
+    # create links to binaries
+    mkdir -p usr/bin
+    ln -sf $nix/bin usr/bin
+    for binary in $nix/bin/*; do
+      ln -sf $binary usr/bin/;
+    done
+
+    # create links to profile initialization files
+    mkdir -p etc/profile.d/
+    ln -sf $nix/etc/profile.d/nix-daemon.sh etc/profile.d/nix-daemon.sh
+    ln -sf $nix/etc/profile.d/nix.sh etc/profile.d/nix.sh
+
+    # create garbage collector root to bootstrapped version of Nix
+    mkdir -p nix/var/nix/gcroots/
+    ln -sf $nix nix/var/nix/gcroots/system_nix
+
+    fpm -s dir -t deb -n nix --deb-no-default-config-files --post-install ${./install.sh} -p $out usr/ etc/ nix/
   ''
 
--- dpkg/install.sh
+++ dpkg/install.sh
@@ -2,34 +2,24 @@
 set -o errexit
 set -o nounset
 
-bootstrap=/usr/local/lib/nix/bootstrap.tar.bz.sh
+bootstrap=/usr/lib/nix/bootstrap.tar.bz.sh
 nix="$($bootstrap --print-packaged-nix-path)"
 
 if ! test -e $nix; then
    ./$bootstrap
 fi
 
+# initialize per-user roots directory.  we create these here because
+# these are stateful and persistent, and shouldn't be deleted if we
+# remove the package.
 mkdir -p --mode=00755 /nix/var/nix/gcroots
 mkdir -p --mode=00755 /nix/var/nix/profiles
 mkdir -p --mode=01777 /nix/var/nix/gcroots/per-user
 mkdir -p --mode=01777 /nix/var/nix/profiles/per-user
 
-# update system_nix garbage collector root to latest version of $nix
-ln -sf $nix /nix/var/nix/gcroots/system_nix
-
-# update systemd file links
-ln -sf $nix/lib/systemd/system/nix-daemon.service /etc/systemd/system/nix-daemon.service
-ln -sf $nix/lib/systemd/system/nix-daemon.socket /etc/systemd/system/nix-daemon.socket
-
+# refresh the systemd services
 systemctl daemon-reload
 systemctl enable nix-daemon.socket
 systemctl start nix-daemon.socket
-# let it get socket activated
+# let nix-daemon get socket activated
 systemctl stop nix-daemon.service
-
-# profile initialization files
-ln -sf $nix/etc/profile.d/nix-daemon.sh /etc/profile.d/nix-daemon.sh
-ln -sf $nix/etc/profile.d/nix.sh /etc/profile.d/nix.sh
-
-# install this version of Nix to the root profile so users can find the Nix tools
-$nix/bin/nix-env -p /nix/var/nix/profiles/default -i $nix
