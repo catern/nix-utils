946b4c4dafeb5aa1f9970aa478e65028c38374b8
George Shammas <george@shamm.as>
2021-04-29 12:59:32 -0400

    Switch to using flakes

--- pkgs.nix
+++ pkgs.nix
@@ -1,4 +1,4 @@
-import ((import ./fetchTarball.nix) {
-  url = https://github.com/NixOS/nixpkgs/archive/e07733ec92267a378b49dd637e80b5bb751f4700.tar.gz;
-  sha256 = "16jn67db9ibcfpak8m75fa45rl0xsb64akwlfkb6lj61733lvic0";
+import ( builtins.fetchTarball {
+  url = https://github.com/NixOS/nixpkgs/archive/8e4fe32876ca15e3d5eb3ecd3ca0b224417f5f17.tar.gz;
+  sha256 = "sha256-R7WAb8EnkIJxxaF6GTHUPytjonhB4Zm0iatyWoW169A=";
 }) {}
